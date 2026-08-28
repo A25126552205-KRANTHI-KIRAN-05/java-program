382155bec0e8b44eea4bce058c2dabb3e38b5de4
A25126552205-KRANTHI-KIRAN-05 <bethakranthikiran.25.csm@anits.edu.in>
2026-08-28 11:04:08 +0530

    TryCatchExample.java

--- lab-practice/TryCAtchExample.java
+++ lab-practice/TryCAtchExample.java
@@ -1,30 +1,33 @@
 import java.util.Scanner;
-class Sample{
+
+class Sample {
     Scanner sc = new Scanner(System.in);
-    void Example(){
-    int x;
-    System.out.println("Enter the value of x: ");
-        x=sc.nextInt();
+
+    void Example() {
+        int x;
+        System.out.println("Enter the value of x: ");
+        x = sc.nextInt();
         int y;
         System.out.println("Enter the value of y: ");
-        y=sc.nextInt();
+        y = sc.nextInt();
 
         try {
-             int z=x/y;
-            System.out.println("z= "+z);
+            int z = x / y;
+            System.out.println("z= " + z);
         } 
         catch (ArithmeticException ae) {
-             System.out.println("Enter value of y must be grater than 0: ");
-             y=sc.nextInt();
-             int z = x/y;
-             System.out.println("z= "+z);
+            System.out.println("Enter value of y must be grater than 0: ");
+            y = sc.nextInt();
+            int z = x / y;
+            System.out.println("z= " + z);
         }
     }
 }
+
 public class TryCAtchExample {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         Sample s = new Sample();
         s.Example();
     }
-    
+
 }
